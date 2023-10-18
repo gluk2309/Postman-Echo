534b5c85db64c1663d151be0c92605970960a4be
gluk2309 <gluk2309@mail.ru>
2023-10-18 11:28:33 +0300

    first version of homework commit

---
+++ src/test/java/ru/netology/postmanExo/PostmanEchoTest.java
@@ -0,0 +1,28 @@
+package ru.netology.postmanExo;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class PostmanEchoTest {
+
+    @Test
+    void returnsSentData() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Hello") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then().log().all()
+                .statusCode(200)
+                .body("data", equalTo("Hello"));
+        ;
+    }
+
+
+}
